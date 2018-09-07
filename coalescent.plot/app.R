72e2e4795a39da39f1d5bff72f5fb800111e6cf8
Liam J. Revell <liam.revell@umb.edu>
2018-09-07 15:50:55 -0300

    app for coalescent.plot

--- coalescent.plot/app.R
+++ coalescent.plot/app.R
@@ -2,18 +2,26 @@ library(shiny)
 library(PopGen)
 
 ui <- fluidPage(
-  sliderInput(inputId="n",
-              label="number of individuals",
-              value=10,min=1,max=100),
-  sliderInput(inputId="ngen",
-              label="number of generations",
-              value=20,min=2,max=100)
-  plotOutput(outputId="coal")
+  sidebarLayout(
+    sidebarPanel(
+     sliderInput(inputId="n",
+                  label="number of individuals",
+                  value=10,min=1,max=100),
+      sliderInput(inputId="ngen",
+                  label="number of generations",
+                  value=20,min=2,max=100),
+      actionButton("newplot", "New plot")
+    ),
+    mainPanel(
+      plotOutput("plot",height="800px")
+    )
+  )
 )
 
 server <- function(input, output) {
-  output$coal<-renderPlot({ 
-    coalescent.plot()
+  output$plot<-renderPlot({
+    input$newplot
+    coalescent.plot(input$n,input$ngen,sleep=0)
   })
 }
 
